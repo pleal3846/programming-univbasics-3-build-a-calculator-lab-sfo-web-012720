df6827e323b7c0c9ada35a59de243f9d10522752
patrick leal <pjleal3846@icloud.com>
2020-01-11 12:28:18 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+num1 + num2 = sum
+return sum 
 end
 
 def subtraction(num1, num2)
-
+num1 - num2 = sum
+return sum
 end
 
 def division(num1, num2)
-
+ num1/num2 = sum
+ return sum
 end
 
 def multiplication(num1, num2)
-
+num1 * num2 = sum
+return sum
 end
 
 def modulo(num1, num2)
-
+num2/num1 = rest 
+return rest
 end
 
 def square_root(num)
-
+math.sqrt(num) = pink
+return pink
 end
