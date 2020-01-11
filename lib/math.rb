aeeb65a4e3cf600fa3a69a40b26e413f69370d80
patrick leal <pjleal3846@icloud.com>
2020-01-11 12:24:17 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,21 @@
 def addition(num1, num2)
-num1 + num2
+num1 + num2 = sum
+return sum 
 end
 
 def subtraction(num1, num2)
-
+num1 - num2 = sum
+return sum
 end
 
 def division(num1, num2)
-
+ num1/num2 = sum
+ return sum
 end
 
 def multiplication(num1, num2)
-
+num1 * num2 = sum
+return sum
 end
 
 def modulo(num1, num2)
